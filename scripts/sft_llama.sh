01e72f8939c8b4d79634de98b27015db257dae73
YC Chen <ycc.tw.email@gmail.com>
2023-08-11 11:14:21 +0800

    0811

---
+++ scripts/sft_llama.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --config_file accelerate_config.yaml  src/train_bash.py \
+    --stage sft \
+    --model_name_or_path /home/mediatek/models/Llama-2-13b-hf \
+    --prompt_template mr_chat \
+    --do_train \
+    --dataset instruct_tc_and_evol \
+    --finetuning_type lora \
+    --output_dir ./outputs/llama_sft \
+    --overwrite_cache \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16 \
+    --dev_ratio 0.0001
